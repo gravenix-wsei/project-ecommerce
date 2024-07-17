b57fde1a36397308a29dc22461ce55e710a0c898
Bartłomiej Talarczyk <41484847+gravenix@users.noreply.github.com>
2024-07-17 21:24:44 +0200

    Issue 12: Create store settings page (#12)

---
+++ src/Entity/StoreSetting.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StoreSettingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: StoreSettingRepository::class)]
+class StoreSetting
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $key = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $value = null;
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): static
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(?string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
